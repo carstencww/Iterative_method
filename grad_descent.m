332df7ec919a504dd9fbe3b75d37e9d325e89339
carstencww <carstencww@gmail.com>
2018-05-07 21:42:32 +0800

    grad descent

--- grad_descent.m
+++ grad_descent.m
@@ -6,7 +6,10 @@ fprintf(1, 'Gradient descend:\n');
 fprintf(1, '#iter\t error\n');
 
 for i = 1 : 50
-  d = A' * b - A' * (A * x);
-  alpha = d' * d / (d' * A' * (A * d));
-  x = x + alpha * d;
-  if i == 25 || i == 50
+	d = A' * b - A' * (A * x);
+	alpha = d' * d / (d' * A' * (A * d));
+	x = x + alpha * d;
+	if i == 25 || i == 50
+		fprintf('%d\t\t%.3e\n', [i, sum(abs(A' * (A * x) - A' * b))]);
+	end
+end
